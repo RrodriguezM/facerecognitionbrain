e8f9eb6194309a95c6bf2db46a926570bd11f661
Rafael <rafael.a.rodriguez.m@gmail.com>
2021-05-29 07:24:44 -0400

    Basic Function

---
+++ src/App.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react'
+import Particles from 'react-particles-js';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: '80fd70077e5d494b82bd857f14b88493'
+});
+
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: 'https://www.publicbooks.org/wp-content/uploads/2019/11/joel-mott-LaK153ghdig-unsplash-scaled-e1574787737429-810x625.jpg',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    console.log(clarifaiFace)
+    const imageFace = document.getElementById('imageFace')
+    const width = imageFace.width
+    const height = imageFace.height
+    console.log(width, height)
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayfaceBox = (box) => {
+    this.setState({ box: box })
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+    this.setState({ input: event.target.value })
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+    app.models.predict("f76196b43bbd45c99b4f3cd8e8b40a8a",
+      this.state.input)
+      .then(response => this.displayfaceBox(this.calculateFaceLocation(response)))
+      .catch(error => {
+        console.log(error)
+      }
+      );
+  }
+
+  render() {
+    return (
+      <div className="App" >
+        <Particles className="particles"
+          params={particleOptions}
+        />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit} />
+        <FaceRecognition imageUrl={this.state.imageUrl} box={this.state.box} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+    return (
+        <div>
+            <p className='f5 white'>
+                {'This Magic Brain will Detect faces in your Pictures. Try it out'}
+            </p>
+            <input className='w-50 shadow-3 br3 bb-1' onChange={onInputChange} />
+            <button className='f6 grow no-underline br-pill ba bw2 ph2 pv1 mb2 dib mid-gray bg-white mh1 pointer' onClick={onButtonSubmit}>Find</button>
+        </div>
+    )
+}
+
+export default ImageLinkForm
